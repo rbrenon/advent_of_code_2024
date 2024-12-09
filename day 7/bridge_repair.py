1b4fe3d67e473d54ff345139b3ecfd5d5262625b
Ryan Brenon <rbrenon@gmail.com>
2024-12-09 08:39:22 -0500

    day 7 p1

--- day 7/bridge_repair.py
+++ day 7/bridge_repair.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 from itertools import product
+import re
 
 
 def main():
     directory = Path.cwd()
 
-    for filename in ['test']:
+    for filename in ['test', 'prod']:
         with Path.open(directory / "day 7" / filename) as file:
             data = file.read().splitlines()
 
-            print(f"Part one ({filename}): {part_one(data)}")
+            print(f"Part one ({filename}): {part_one(data)}")   # 5512534574980
 
 
 def part_one(data:list[str]) -> int:
@@ -20,25 +21,26 @@ def part_one(data:list[str]) -> int:
         answer = int(answer)
         values = [int(val) for val in values.strip().split()]
 
-        if find_valid_combo(answer=answer, values=values):
-            valid_equations.extend(values)
-
-        print(f"{answer=} {values=}")
+        if valid := find_valid_combo(answer=answer, values=values):
+            valid_equations.append(valid)
 
     return sum(valid_equations)
 
 
 def find_valid_combo(answer: int, values: list[int]):
     number_of_operators = len(values) - 1
-
     operators_to_try = list(product("*+", repeat=number_of_operators))
 
-    for operator in operators_to_try:
-        # interleaved_list = list(*zip(values), operators_to_try)
-
-        print(f"{operator=} - {operators_to_try=}")
-
-    
+    for operators in operators_to_try:
+        equation = str()
+        for i, value in enumerate(values):
+            equation += str(value)
+            equation = eval(equation)
+            try:
+                equation = str(equation) + operators[i]
+            except IndexError as ie:
+                if equation == answer:
+                    return answer
 
 
 if __name__ == "__main__":
