e37e3ccb8f0350776c0aee70a215a48e1b332215
Ryan Brenon <rbrenon@gmail.com>
2024-12-14 12:54:25 -0500

    day 9 p1 - partial

--- day 9/disk_fragmenter.py
+++ day 9/disk_fragmenter.py
@@ -4,55 +4,61 @@ from pathlib import Path
 def main():
     directory = Path.cwd()
 
-    for filename in ["test"]:#, "prod"]:
+    for filename in ["test", "prod"]:
         with Path.open(directory / "day 9" / filename) as file:
             data = file.read()
 
-            print(f"Part 1 ({filename}): {part_one(data)}") # 90033860628 - too low
+            print(f"Part 1 ({filename}): {part_one(data)}") # 90,033,860,628 - too low, 6,310,938,013,104 - too high
 
 
 def part_one(data: str) -> int:
     block_map = create_block_map(data)
-    print(f"{block_map=}")
+    # print(f"{block_map=} \n {len(block_map)=}")
     defragged_block_map = defrag_block_map(block_map)
-    print(f"{defragged_block_map=}")
+    # print(f"{defragged_block_map=}")
     answer = calculate_checksum(defragged_block_map)
-    print(f"{answer=}")
+    # print(f"{answer=}")
     return answer
 
 
-def calculate_checksum(defragged_block_map: str) -> int:
+def calculate_checksum(defragged_block_map: list[tuple[int, int]]) -> int:
     return sum(
         a * b
-        for a, b in zip(
-            [int(x) for x in defragged_block_map], list(range(len(defragged_block_map)))
-        )
+        for a, b in defragged_block_map
     )
 
 
-def defrag_block_map(block_map: str) -> str:
-    defragged_block_map = str()
-    block_map = [ch for ch in block_map]
+# todo 1; figure out how to use list vs string
+def defrag_block_map(block_map: list) -> str:
+    defragged_block_map = list()
 
     for i, block in enumerate(block_map):
-        while block == ".":
-            block_map[i] = block_map.pop(-1)
-            block = block_map[i]
-
-        defragged_block_map += block
+        if "." in block:
+            new_block = block_map.pop(-1)
+            while "." in new_block:
+                new_block = block_map.pop(-1)
+            defragged_block_map.append((i, new_block[1]))
+        else:
+            defragged_block_map.append(block)
 
     return defragged_block_map
 
 
-def create_block_map(data: str) -> str:
-    block_map = str()
+def create_block_map(data: str) -> list:
+    block_map = list()
+    block_no = 0
 
     for i, block in enumerate(data):
         if i % 2 == 0:
-            block_map += str(i // 2) * int(block)
+            # block will tell how many blocks for the file
+            for b in range(int(block)):
+                block_map.append((block_no, i // 2))
+                block_no += 1
         else:
-            block_map += "." * int(block)
-
+            # block will be how many open blocks until the next file
+            for dot in range(int(block)):
+                block_map.append((block_no, "."))
+                block_no += 1
     return block_map
 
 
